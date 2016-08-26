c13470085c9cfc726f764fb551ae9a643d1e2b45
Yusuf Qedan <yusuf9191@gmail.com>
2016-08-26 08:41:03 -0700

    add form input for pizza order and created pizza object to store form input data

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
+//Business Logic
+function Pizza(size,toppings){
+  this.sizeOfpizza = size;
+  this.toppings = toppings;
+}
+//UI logic
 $(document).ready(function() {
+  $("form").submit(function(event){
+    event.preventDefault();
+    var size = $("input:radio[name=size]:checked").val();
+    var toppings = [];
+    $("input[name=toppings]:checked").each(function() {
+      toppings.push(this.value);
+    });
+    var myPizza = new Pizza(size,toppings);
+    alert(myPizza.sizeOfpizza);
+    alert(myPizza.toppings.length);
+  });
 
 });
